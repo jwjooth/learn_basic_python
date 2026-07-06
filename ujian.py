2e53526739e267434b639c89af537d6f85a70901
Jabez chrisopras ferdinand <jabeschrisopras@gmail.com>
2026-07-06 19:35:36 +0800

    Update ujian.py

--- ujian.py
+++ ujian.py
@@ -217,35 +217,176 @@
 #       print('Error: Pembagian dengan nol tidak diperbolehkan.')
 
 # print('=== OPERATOR SELESAI ===')
-history = []
-def TebakAngka():
-    import random
-    angka_rahasia = random.randint(1,10)
-    percobaan = 0
-    batas_percobaan = 10
+from unicodedata import name
+
+
+# history = []
+# def TebakAngka():
+#     import random
+#     angka_rahasia = random.randint(1,10)
+#     percobaan = 0
+#     batas_percobaan = 10
     
-    print('selamat datang di permainan tebak angka')
-    while percobaan < batas_percobaan:
-        tebakan = int(input('masukan angka antara 1 sampai 10 :'))
-        history.append(tebakan)
-        if tebakan < angka_rahasia:
-            percobaan += 1
-            print('terlalu rendah')
-        elif tebakan > angka_rahasia:
-            percobaan += 1
-            print('terlalu tinggi')
-        else:
-            print(f'selamat tebakan anda benar {angka_rahasia}')
-            break
-    print(f'anda telah mencoba {percobaan} kali, angka rahasia adalah {angka_rahasia}')
-    print('skor permainan tebak angka :')
-    print(f'history percobaan = {history}')
-    if percobaan == 3 and percobaan <= 3:
-         print('skor anda = 100')
-    elif percobaan >= 4 and percobaan <= 5:
-         print('skor anda = 75')
-    elif percobaan >= 6 and percobaan <= 7:
-         print('skor anda = 50')
+#     print('selamat datang di permainan tebak angka')
+#     while percobaan < batas_percobaan:
+#         tebakan = int(input('masukan angka antara 1 sampai 10 :'))
+#         history.append(tebakan)
+#         if tebakan < angka_rahasia:
+#             percobaan += 1
+#             print('terlalu rendah')
+#         elif tebakan > angka_rahasia:
+#             percobaan += 1
+#             print('terlalu tinggi')
+#         else:
+#             print(f'selamat tebakan anda benar {angka_rahasia}')
+#             break
+#     print(f'anda telah mencoba {percobaan} kali, angka rahasia adalah {angka_rahasia}')
+#     print('skor permainan tebak angka :')
+#     print(f'history percobaan = {history}')
+#     if percobaan == 3 and percobaan <= 3:
+#          print('skor anda = 100')
+#     elif percobaan >= 4 and percobaan <= 5:
+#          print('skor anda = 75')
+#     elif percobaan >= 6 and percobaan <= 7:
+#          print('skor anda = 50')
+#     else:
+#          print('skor anda = 25')
+         
+# TebakAngka()
+
+# db_mahasiswa = {}
+# def tambah_mahasiswa(nama,nim,semestar,gpa,):
+#     if nim in db_mahasiswa:
+#        raise ValueError(f'Mahasiswa dengan NIM {nim} sudah ada dalam database.')
+#     else:
+#         db_mahasiswa[nim] = (nama, semestar, gpa)
+#         print(f'{nim} - {nama} - (semester{semestar} -  gpa{gpa} ditambahkan')
+
+# def tampilkan_mahasiswa():
+#     nama = input('masukan nama mahasiswa yang ingin di tampilkan :')
+#     if nama not in [mahasiswa[0] for mahasiswa in db_mahasiswa.values()]:
+#         print(f'Mahasiswa dengan nama {nama} tidak ditemukan dalam database.')
+#     else:
+#         print('Daftar Mahasiswa :')
+#         for nim, (nama, semester, gpa) in db_mahasiswa.items():
+#             print(f'NIM: {nim}, Nama: {nama}, Semester: {semester}, GPA: {gpa}')
+
+# def update_gpa(nim, gpa_baru,db):
+#     nama,semester,gpa = db[nim]
+#     db[nim] = (nama, semester, gpa_baru)
+#     print(f'GPA mahasiswa dengan NIM {nim} dari {gpa} berhasil diperbarui menjadi {gpa_baru}.')
+
+
+# def cari_mahasiswa(min_gpa ,db):
+#     for nim, (nama, semester, gpa) in db.items():
+#         if gpa >= min_gpa:
+#             print(f'Mahasiswa dengan GPA {gpa} ditemukan:')
+#             print(f'NIM: {nim}, Nama: {nama}, Semester: {semester}, GPA: {gpa}')
+
+# def statistik_gpa(db):
+#     if not db:
+#         print('Database mahasiswa kosong.')
+#         return
+#     else:
+#         gpa_list = [gpa[2] for gpa in db.values()]
+#         print(f'Statistik GPA:')
+#         print(f'GPA Tertinggi: {max(gpa_list)}')
+#         print(f'GPA Terendah: {min(gpa_list)}')
+#         print(f'GPA Rata-rata: {sum(gpa_list) / len(gpa_list):.2f}')
+
+# def hapus_mahasiswa(nim, db):
+#     if nim in db:
+#         del db[nim]
+#         print(f'Mahasiswa dengan NIM {nim} berhasil dihapus dari database.')
+
+
+# tambah_mahasiswa('budi', '12345', 3 , 3.5)
+# tampilkan_mahasiswa()
+# update_gpa('12345', 3.8, db_mahasiswa)
+# statistik_gpa(db_mahasiswa)
+# cari_mahasiswa(3.0, db_mahasiswa)
+# hapus_mahasiswa('12345', db_mahasiswa)
+
+
+# with open('data_penjualan.txt', 'w') as file:
+#     while True:
+#         produk = input('Masukkan nama produk: ')
+#         harga = float(input('Masukkan harga produk: '))
+#         jumlah = int(input('Masukkan jumlah produk: '))
+#         file.write(f' {produk}, {harga}, {jumlah}, {harga * jumlah}\n')
+#         print(f'Data Penjualan {produk} berhasil disimpan ke file data_penjualan.txt')
+#         lanjut = input('Apakah Anda ingin menambahkan data penjualan lagi? (y/n): ')
+#         if lanjut.lower() != 'y':
+#                 break
+
+def hitung_bunga_majemuk(principal, rate, time, n=1):
+    rate = rate / 100
+    jumlah_akhir = principal * (1 + rate / n) ** (n * time)
+    bunga_total = jumlah_akhir - principal
+    return jumlah_akhir, bunga_total
+
+def validasi_input(principle, rate, time):
+    if principle <= 0:
+        return False, 'modal harus lebih dari 0'
+    if rate <= 0 :
+        return False, 'suku bunga harus lebih dari 0'
+    if time <= 0 :
+        return False, 'waktu investasi harus lebih dari kosong'
     else:
-         print('skor anda = 25')
-TebakAngka()
+        return True, ""
+def banding_investasi(principle, rate , time):
+    hasil_dict = {}
+
+    for modal in principle:
+        akhir, bunga = hitung_bunga_majemuk(modal,rate,time)
+        hasil_dict[modal] = {
+            'jumlah_akhir' : akhir,
+            'bunga' : bunga
+        }
+
+    investasi_terbaik = max(
+        hasil_dict,
+        key= lambda x : hasil_dict[x] ['jumlah_akhir']
+    )
+    return hasil_dict,investasi_terbaik
+
+def generate_laporan(nama_investasi, principal,final_amount, bunga):
+    laporan = f"""
+  === {nama_investasi} ===
+  Modal Awal : Rp {principal:.2f}
+  Jumlah Akhir : Rp {final_amount:.2f}
+  keuntungan : Rp {bunga:.2f}
+  Roi        : Rp {(bunga / principal)* 100:.2f}"""
+    return laporan
+
+def menu():
+    print('=== KALKULATOR INVESTASI ===')
+    principal =float(input('masukan jumlah investasi awal : Rp'))
+    rate = float(input('masukan suku bunga tahunan (&) : '))
+    time = float(input('masukan jangka waktu investasi (tahun) :'))
+
+    valid,pesan = validasi_input(principal,rate,time)
+
+    if not valid:
+        print(pesan)
+    else:
+        akhir, bunga = hitung_bunga_majemuk(principal,rate,time)
+
+        print('== ANALISI INVESTASI ANDA ==')
+        print(generate_laporan('investasi anda',principal, rate, time))
+        
+        pilihan = [10000000, 15000000, 20000000]
+
+        hasil, terbaik = banding_investasi(pilihan, rate, time)
+
+        print('== PERBANDINGAN INVESTASI ==')
+        for modal, data in hasil.items():
+            print(generate_laporan(
+            f'modal Rp {modal:.2f}',
+            modal,
+            data['jumlah_akhir'],
+            data['bunga'],
+          ))
+            
+        print(f'investasi terbaik : Rp {terbaik :.2f}')
+menu()
